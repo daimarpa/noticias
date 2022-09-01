b87992c879f8af0cb002ab9147454adf1ea08242
DAIMAR ALONDRA PARGAS ALMAO <DAIMAR.PARGAS@correoaiep.cl>
2022-09-01 00:18:19 -0400

    version casi lista

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
-
+import { ActionSheetController } from '@ionic/angular';
+import{Share} from '@capacitor/share'
 
 @Component({
   selector: 'app-article',
@@ -13,13 +14,50 @@ export class ArticleComponent implements OnInit {
   @Input() article: Article;
   @Input() index:number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser, private actionSheetCtrl:ActionSheetController) { }
 
   ngOnInit() {}
 
   openArticle(){
     const browser = this.iab.create(this.article.url);
     browser.show();
+  }
+
+   async openMenu(){
+
+const actionSheet = await this.actionSheetCtrl.create({
+
+header : 'options',
+buttons :[
+  {
+  text:'Share',
+  icon:'share-outline',
+  handler:()=>this.shareArticle()
+  },
+  {
+    text:'Favorites',
+    icon:'Heart-outline',
+    handler:()=>this.onToggleFavorites()
+    },
+    {
+      text:'Cancel',
+      icon:'Close-outline',
+      role:'cancel'
+      }
+]
+});
+
+await actionSheet.present();
+
+  }
+
+  shareArticle(){
+console.log('share article')
+
+  }
+
+  onToggleFavorites(){
+    console.log('favorites')
   }
 
 }
